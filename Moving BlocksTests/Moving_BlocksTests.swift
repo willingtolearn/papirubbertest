2267e102e062a3cf01ca9fdba320044ae3b6b6dd
greyintel <kevinmunck37@gmail.com>
2020-04-19 19:17:08 -0400

    lets do it!
    lets do it

---
+++ Moving BlocksTests/Moving_BlocksTests.swift
@@ -0,0 +1,34 @@
+//
+//  Moving_BlocksTests.swift
+//  Moving BlocksTests
+//
+//  Created by kevin munck  on 4/19/20.
+//  Copyright © 2020 greyintel. All rights reserved.
+//
+
+import XCTest
+@testable import Moving_Blocks
+
+class Moving_BlocksTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
